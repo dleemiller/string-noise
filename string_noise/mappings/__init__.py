eeb8c6b594783a595b5755ba8d557022d83a500b
Lee Miller <dlmiller@adventhp.com>
2023-12-24 20:03:23 -0700

    mapping class

--- string_noise/mappings/__init__.py
+++ string_noise/mappings/__init__.py
@@ -1,1 +1,27 @@
+import functools
+
 from .mapper import Mapper
+
+RESOURCE_PACKAGE = "string_noise.mappings.default"
+
+
+class CustomMapper:
+    @classmethod
+    def new(cls, path):
+        return functools.partial(
+            Mapper.load, resource_package=RESOURCE_PACKAGE, resource_path=path
+        )
+
+
+load_leet = CustomMapper.new("leet.json")
+load_homoglyph = CustomMapper.new("homoglyphs.json")
+load_phonetic = CustomMapper.new("phonetic.json")
+load_keyboard = CustomMapper.new("keyboard.json")
+
+__all__ = [
+    "CustomMapper",
+    "load_leet",
+    "load_homoglyph",
+    "load_phonetic",
+    "load_keyboard",
+]
